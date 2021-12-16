82f6d635a16384a00dd9b5a481123b1bdc3491f7
Matthew Hayes <matthewha853@gmail.com>
2021-12-15 18:09:25 -0800

    do as much of the landing page as possible

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="section-grid min-h">
+  <div class="flex flex-col">
+    <h1 class="font-raleway text-7xl">Stoma</h1>
+    <p class="text-xl mt-2">End Your Arguments And Find A Place To Eat.</p>
+    <p class="text-secondary mt-2">
+      Whether you're out with friends, or on your own in an unfamiliar place, Stoma will handle your
+      food search problem, and then you'll be able to satisfy your hunger.
+    </p>
+    <a
+      class="mt-4"
+      href="https://play.google.com/store/?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+    >
+      <img
+        class="google-play-btn"
+        alt="Get it on Google Play"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+      />
+    </a>
+  </div>
+  <div>
+    <h1>Image to be made</h1>
+  </div>
+</section>
+
+<style>
+  .section-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media screen and (max-width: 768px) {
+    .section-grid {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  .min-h {
+    min-height: 70vh;
+  }
+
+  .google-play-btn {
+    max-width: 256px;
+  }
+
+  @media screen and (max-width: 400px) {
+    .google-play-btn {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+</style>
